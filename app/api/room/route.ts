5188b8f20f39780dac51c8e0a362531398071f02
Eigkivz <kakuppachai_me@hotmail.com>
2026-03-05 10:14:38 +0700

    add get rooms type success

--- app/api/room/route.ts
+++ app/api/room/route.ts
@@ -2,6 +2,33 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { z } from "zod";
 
+export async function GET(
+    req: Request,
+    { params }: { params: Promise<{roomTypeId: string}>}
+) {
+    try {
+        const { roomTypeId } = await params;
+        const rooms =await prisma.room.findMany({
+            orderBy: {
+                createdAt: "desc"
+            },
+            include: {
+                roomType: true
+            },
+            where: {
+                id: roomTypeId
+            }
+        });
+
+         return NextResponse.json(rooms);
+    } catch (error) {
+        return NextResponse.json(
+            { error: (error as Error).message},
+            { status: 500}
+        )
+    }
+}
+
 export async function POST(req: Request) {
     try {
         const body = await req.json();
